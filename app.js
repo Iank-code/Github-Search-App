6b7c3a99b903b90a02ccbe18511e0057ee9bac64
ianK-code <kian99564@gmail.com>
2022-12-14 10:33:46 +0300

    Updating fetch and catching any errors that may arise

--- app.js
+++ app.js
@@ -12,21 +12,19 @@ window.addEventListener('DOMContentLoaded', ()=>{
     //         }
     //     })
     // })
-    const GITHUB_USERS = 'https://api.github.com/users'
+    // const GITHUB_USERS = 'https://api.github.com/users/${}
     const searchBar = document.getElementById('seachBar')
     const searchBtn = document.getElementById('searchUser')
     searchBtn.addEventListener('click', ()=>{
         const searchedName = searchBar.value
-        console.log(searchedName)
 
-        const fetchUser = fetch('/db.json', {
+        const fetchUser = fetch(`https://api.github.com/users/${searchedName}`, {
             method: 'GET',
             headers: {Accept: 'application/json'}
         })
         fetchUser.then(res => res.json()).then(
             data => console.log(data)
-        )
-        console.log(`https://api.github.com/search/users?q=mojombo`)
+        ).catch(error => console.log(error))
     })
 
 })
