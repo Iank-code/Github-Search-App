bf165e9d95f705d87a63863b11ea3377381c6d58
ianK-code <kian99564@gmail.com>
2022-12-15 18:30:13 +0300

    Changing a few html, css and js things

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 
 window.addEventListener('DOMContentLoaded', ()=>{
-    
+    //global variables
     const githubSection = document.getElementById('github-search-container')
-    const loginSection = document.getElementById('login-signup')
+    // variables for the github-search section
+    const searchBar = document.getElementById('seachBar')
+    const searchBtn = document.getElementById('searchUser')
+    const userImg = document.getElementById('userImg')
+    const follower = document.getElementById('follower')
+    const following = document.getElementById('following')
+    const company  = document.getElementById('company')
+    // Navlinks variables
+    const login = document.getElementById('loginLink')
+    login.textContent = 'Login'
+    const signUp = document.getElementById('signUpLink')
+    signUp.textContent = 'Sign Up'
+    const about = document.getElementById('about')
+    const signOff = document.getElementById('signOut')
+    
+    // SignUp section variables
+    const signUpForm = document.getElementById('signUp')
+    
 
+    // login section variables
+    const loginSection = document.getElementById('login')
+    const password = document.getElementById('password')
+    const userName = document.getElementById('userName')
+    const loginForm = document.getElementById('loginForm')
     // Sign out button works
     const signOut = document.getElementById('signOut')
     signOut.style.cursor = 'pointer'
@@ -12,27 +34,36 @@ window.addEventListener('DOMContentLoaded', ()=>{
     })
 
 
-    // for login page
-    const password = document.getElementById('password')
-    const form = document.getElementById('form')
-    form.addEventListener('click', (e)=>{
+
+    // for login page && it works
+    loginForm.addEventListener('submit', (e)=>{
         e.preventDefault();
+        document.getElementById('loginBtn').addEventListener('click', (e)=>{
+            const fetchCredentials = fetch('/credentials.json')
+            fetchCredentials.then(res => res.json()).then(
+                data => {
+                    
+                    if(userName.value === data[0].username && password.value === data[0].password){
+                        signOff.textContent = "Log Out"
+                        signUp.textContent = ""
+                        login.textContent = ""
+                        about.textContent = "About"
+                        signUpForm.classList.add('hide')
+                        loginSection.classList.add('hide')
+                        githubSection.classList.remove('hide')
+                    }else{
+                        console.log('not working')
+                        return false
+                    }}
+            )
+            
+        })
     })
-    document.getElementById('loginBtn').addEventListener('click', (e)=>{
-        // githubSection.classList.remove('hide')
-        loginSection.classList.add('hide')
-        githubSection.classList.remove('hide')
-        
-    })
 
-
-
-    const searchBar = document.getElementById('seachBar')
-    const searchBtn = document.getElementById('searchUser')
-    const userImg = document.getElementById('userImg')
-    const follower = document.getElementById('follower')
-    const following = document.getElementById('following')
-    const company  = document.getElementById('company')
+    /* when the search button is clicked, it takes in the
+    named of the person being searched on and search it. It then displays 
+    the image,followers of the person, the company he/she works
+    for and the amount of people following him/her */
     searchBtn.addEventListener('click', ()=>{
         const searchedName = searchBar.value
 
